ab84c71bff2fc075d4e9f05cbd947856c4ddb560
Abdzsam <127457343+Abdzsam@users.noreply.github.com>
2024-09-03 15:02:18 -0300

    Final Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import logo from "../assets/PortLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between">
@@ -8,8 +10,15 @@ const Navbar = () => {
             <img className="mx-2 w-20 rounded-full" src={logo} alt="logo"/>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin className="text-[#b4dbdc] size-10"></FaLinkedin>
-            <FaGithub className="text-[#b4dbdc] size-10"></FaGithub>
+        <a href="https://www.linkedin.com/in/abdsam04/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin className="text-[#b4dbdc] size-10" />
+            </a>
+            <a href="https://github.com/Abdzsam" target="_blank" rel="noopener noreferrer">
+                <FaGithub className="text-[#b4dbdc] size-10" />
+            </a>
+            <a href="https://leetcode.com/u/abdzsam/" target="_blank" rel="noopener noreferrer">
+                <SiLeetcode className="text-[#b4dbdc] size-10" />
+             </a>   
         </div>
     </nav>
   )
